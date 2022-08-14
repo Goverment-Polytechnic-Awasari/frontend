2f0d5619111273f0336b5604c71baec9384c81dd
Pranav <pranavshelke4@gmail.com>
2022-08-14 23:25:41 +0530

    route add to home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
+import UserRoute from "./components/UserRoute";
 
 // Priyanka Ghode Routes.....
 
 import Dashbord from "./pages/SubjectTeacherPages/PriyankaGhode/Dashbord";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Login} />
           <Route path="/ST-Dashbord" component={Dashbord} />
+          <UserRoute path="/home" component={Home} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/LoadingToRedirect.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+const LoadingToRedirect = () => {
+  const [count, setCount] = useState(1);
+  const history = useHistory();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((currentCount) => --currentCount);
+    }, 1000);
+
+    count === 0 && history.push("/");
+    return () => clearInterval(interval);
+  }, [count, history]);
+
+  return (
+    <div>
+      <p>Redirecting you in {count} seconds</p>
+    </div>
+  );
+};
+
+export default LoadingToRedirect;
