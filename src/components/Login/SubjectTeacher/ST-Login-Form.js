a73da083edd8e616793d2de766108844cc7d6d8f
Pranav <pranavshelke4@gmail.com>
2022-08-14 20:32:38 +0530

    login done

---
+++ src/components/Login/SubjectTeacher/ST-Login-Form.js
@@ -0,0 +1,72 @@
+import { React, useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';    
+import { loginInitiate } from '../../../redux/actions';
+import "./ST-Login-Form.css"
+
+function STLoginForm() {
+
+    const [state, setState] = useState({
+        email: '',
+        password: '',
+    });
+    
+    const { email, password } = state;
+
+    const { currentUser } = useSelector(state => state.user);
+
+    const history = useHistory();
+
+    useEffect(() => {
+        if (currentUser) {
+            history.push('/home');
+        }
+    },[currentUser, history]);
+
+    const dispatch = useDispatch();
+
+    const handelSubmit = (e) => {
+        e.preventDefault();
+        if(!email || !password) {
+            return;
+        }
+        dispatch(loginInitiate(email, password));
+        setState({...state, email: '', password: ''});
+    }
+    const hnadleChange = (e) => {
+        let { name, value } = e.target;
+        setState({...state, [name]: value});
+    }
+
+  return (
+    <div className='box'>
+        <form onSubmit={handelSubmit}>
+        <label htmlFor="email">Email ID / Username</label>
+        <input
+          type="email"
+          name="email"
+          id="STemail"
+          placeholder="Email ID / Username"
+          onChange={hnadleChange}
+          value={email}
+          autoFocus
+          required
+          className='inputBox'
+        />
+        <label htmlFor="password">Password</label>
+        <input 
+         type="password" 
+         name="password"
+         id="STpassword"
+         placeholder="Enter Password"
+         value={password}
+         onChange={hnadleChange}
+         className='inputBox'
+         required />
+        <button type="submit" className="login-btnss">Login</button>
+        </form> 
+    </div>
+  )
+}
+
+export default STLoginForm
---
+++ src/redux/actions.js
@@ -0,0 +1,56 @@
+import * as types from "./actionTypes";
+import { auth } from "../firebase";
+
+//login
+const loginStart = () =>({
+    type:types.LOGIN_START,
+});
+
+const loginSuccess = (user) =>({
+    type:types.LOGIN_SUCCESS,
+    payload:user
+});
+
+const loginFail = (error) =>({
+    type:types.LOGIN_FAIL,
+    payload:error
+});
+//logout
+const logoutStart = () =>({
+    type:types.LOGOUT_START,
+});
+
+const logoutSuccess = () =>({
+    type:types.LOGOUT_SUCCESS,
+});
+
+const logoutFail = (error) =>({
+    type:types.LOGOUT_FAIL,
+    payload:error
+});
+
+export const setUser = (user) =>({
+    type:types.SET_USER,
+    payload:user,
+})
+
+export const  loginInitiate = (email,password)=>{
+    return function(dispatch){
+        dispatch(loginStart());
+        auth
+            .signInWithEmailAndPassword(email,password)
+            .then(({user})=>{
+                dispatch(loginSuccess(user));
+              })
+        .catch((error)=>dispatch(loginFail(error.message)));
+    }
+}
+
+export const  logoutInitiate = ()=>{
+    return function(dispatch){
+        dispatch(logoutStart());
+        auth.signOut().then((res)=>{
+            dispatch(logoutSuccess());
+        }).catch((error)=>dispatch(logoutFail(error.message)));
+    }
+}
